91dbf22323d131513daa5b7f9ccffffee78b3cf5
simon.leitl <simon.leitl@t-online.de>
2020-01-13 17:54:14 +0100

    Klassen und Paketstruktur hinzugefügt

---
+++ SPC/Components/Component.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPC.Components
+{
+   public abstract class Component
+    {
+    }
+}
