161daa129213b249a880e7a76fe83cf5175190d8
simon.leitl <simon.leitl@t-online.de>
2020-01-14 10:03:39 +0100

    Logik für das erstellen von neuen Projekten - Methode für das erstellen von neuen Projekten - Methode für das automatische erstellen von neuem Ordner

--- SPC/Start_Menu/New_Project.cs
+++ SPC/Start_Menu/New_Project.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SPC.Start_Menu
 {
     class New_Project
     {
+        private String name;
+        public New_Project(String name)
+        {
+            this.name = name;
+            createNewProject();
+        }
+
+        //Methode mit der das neue Projekt angelegt wird. Überprüft zunächst ob der Ordner zum Speichern vorhanden ist. Ansonsten wird ein neuer erstellt. 
+        public void createNewProject()
+        {
+            if (checkDirectory() == true)
+            {
+                createFile();
+            }
+            else
+            {
+                createDirectory();
+            }
+        }
+
+        //Methode die Überprüft ob der Ordner, in welchem die Dateien gespeichert werden, vorhanden ist
+        public Boolean checkDirectory()
+        {
+            if (Directory.Exists("Saving"))
+            {
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Erstellt den neuen Ordner zum Speichern der Datei
+        public void createDirectory()
+        {
+            Directory.CreateDirectory("Savings");
+        }
+        
+        //Erstellt das Datei Format
+        public void createFile()
+        {
+
+        }
     }
 }
