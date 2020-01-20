bc71296696b8a92608860a77ae9fda7a3b34043c
MJ7898 <40462862+MJ7898@users.noreply.github.com>
2020-01-20 16:06:01 +0100

    Ui zum Projekt erstellen errichtet

---
+++ SPC/ProjektNameEingabe.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SPC
+{
+    /// <summary>
+    /// Interaktionslogik für ProjektNameEingabe.xaml
+    /// </summary>
+    public partial class ProjektNameEingabe : Window
+    {
+        public ProjektNameEingabe()
+        {
+            InitializeComponent();
+        }
+    }
+}
