be061e513b28fc590b6f411470e3cd30d0619547
simon.leitl <simon.leitl@t-online.de>
2020-02-11 14:42:00 +0100

    MVVM Struktur hinzugefügt

--- SPC/SPC.Einstellungen/ViewModels/EinstellungenViewModel.cs
+++ SPC/SPC.Einstellungen/ViewModels/EinstellungenViewModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SPC
+namespace SPC.SPC.Einstellungen.ViewModels
 {
-    class Settings
+    class EinstellungenViewModel
     {
     }
 }
