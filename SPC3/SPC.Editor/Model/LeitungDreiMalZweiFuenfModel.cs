9543cc9e61f12a4354fa700cad24257400936e88
MJ7898 <40462862+MJ7898@users.noreply.github.com>
2020-02-25 15:34:32 +0100

    Create LeitungenToolsModel, LeitungenToolsViewModel and Leitungen

---
+++ SPC3/SPC.Editor/Model/LeitungDreiMalZweiFuenfModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPC3.SPC.Editor.Model
+{
+    class LeitungDreiMalZweiFuenfModel : LeitungenToolsModel
+    {
+        private new String LeitungenName = "3x 2.5";
+        private new int LeitungenID = 1;
+    }
+
+}
---
+++ SPC3/SPC.Editor/Model/LeitungenToolsModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SPC3.SPC.Editor.Model
+{
+    abstract class LeitungenToolsModel
+    {
+        private String _LeitungenName;
+        public String LeitungenName{
+            get => _LeitungenName;
+            set => _LeitungenName = value;
+        }
+
+        private Int16 _LeitungenID;
+        public short LeitungenID
+        {
+            get => _LeitungenID;
+            set => _LeitungenID = value;
+        }
+
+        private String _LeitungenGroesse;
+        public String LeitungenGroesse
+        {
+            get => _LeitungenGroesse;
+            set => _LeitungenGroesse = value;
+        }
+
+        private Image _LeitungenImage;
+        public Image LeitungenImage
+        {
+            get => _LeitungenImage;
+            set => _LeitungenImage = value;
+        }
+
+        private double _LeitungenXPosition;
+        public double LeitungenXPosition
+        {
+            get => _LeitungenXPosition;
+            set => _LeitungenXPosition = value;
+        }
+
+        private double _LeitungenYPosition;
+        public double LeitungenYPosition
+        {
+            get => _LeitungenYPosition;
+            set => _LeitungenYPosition = value;
+        }
+    }
+}
