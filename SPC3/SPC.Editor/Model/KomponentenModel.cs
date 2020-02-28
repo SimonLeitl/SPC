6fdf7298f8d55aa6b799d449ca9367801a8636f6
simon.leitl <simon.leitl@t-online.de>
2020-02-28 10:00:01 +0100

    SPC KomponentenModel & ViewModels hinzugefügt

---
+++ SPC3/SPC.Editor/Model/KomponentenModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SPC3.SPC.Editor.Model
+{
+     abstract class KomponentenModel
+    {
+
+        public KomponentenModel()
+        {
+        }
+
+        public String komponentenName;
+        private String komponentenBeschreibung;
+        private Image komponentenImage;
+
+        public String KomponentenName
+        {
+            get => komponentenName;
+            set => komponentenBeschreibung = value;
+        }
+
+
+        public String KomponentenBeschreibung
+        {
+            get => komponentenBeschreibung;
+            set => komponentenBeschreibung = value;
+        }
+
+        public Image Image
+        {
+            get => komponentenImage;
+            set => komponentenImage = value;
+        }
+    }
+}
---
+++ SPC3/SPC.Editor/ViewModel/SteckdoseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPC3.SPC.Editor.Model;
+
+namespace SPC3.SPC.Editor.ViewModel
+{
+    public class SteckdoseViewModel : KomponentenViewModel
+    {
+        SteckdoseModel steckdoseModel = new SteckdoseModel();
+        public SteckdoseViewModel()
+        {
+            Name = steckdoseModel.komponentenName;
+        }
+    }
+}
