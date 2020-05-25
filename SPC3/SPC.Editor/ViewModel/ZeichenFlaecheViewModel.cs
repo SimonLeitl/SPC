28f7d739758296ae17000896d8cb00e17457626b
MJ7898 <40462862+MJ7898@users.noreply.github.com>
2020-05-25 21:20:13 +0200

    Added function to get koordinates form canvas

--- SPC3/SPC.Editor/ViewModel/ZeichenFlaecheViewModel.cs
+++ SPC3/SPC.Editor/ViewModel/ZeichenFlaecheViewModel.cs
@@ -8,26 +8,28 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Shapes;
 using SPC3.SPC.Editor.Views;
-using System.Drawing;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 
+using System.Windows;
+using System.Windows.Media;
+
 namespace SPC3.SPC.Editor.ViewModel
 {
-   public class ZeichenFlaecheViewModel : INotifyPropertyChanged
+    public class ZeichenFlaecheViewModel : INotifyPropertyChanged
     {
+        private MouseBehaviour mouseBehaviour = new MouseBehaviour();
         private double _panelX;
         private double _panelY;
-        private ArrayList _mousePosition = new ArrayList();
-
+        private Point[] mousePosition = new Point[2];
+        private System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Black,3);
         public double PanelX
         {
-            get { return _panelX; }
+            get { return Math.Round(_panelX); }
             set
             {
                 if (value.Equals(_panelX)) return;
@@ -38,7 +40,7 @@ namespace SPC3.SPC.Editor.ViewModel
 
         public double PanelY
         {
-            get { return _panelY; }
+            get { return Math.Round(_panelY); }
             set
             {
                 if (value.Equals(_panelY)) return;
@@ -63,32 +65,103 @@ namespace SPC3.SPC.Editor.ViewModel
                 throw new ArgumentNullException(GetType().Name + " does not contain property: " + propertyName);
         }
 
-        private void Canvas_MouseDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        public ICommand GetKooradinatesClickOnCanvas
         {
-            if (e.ButtonState == MouseButtonState.Pressed)
-            {
-                _mousePosition.Add(_panelX);
-                _mousePosition.Add(_panelY);
-            }
-
-            if (e.ClickCount == 2)
-            {
-                _mousePosition.Add(_panelX);
-                _mousePosition.Add(_panelY);
-
-            }
-               
+            get { return new RelayCommand<EventArgs>(MouseClick_ToDraw); }
         }
 
-       private void DrawLine() { 
-                Line line = new Line();
+        public ICommand DrawLineClickOnCanvas
+        {
+            get { return new RelayCommand<System.Windows.Forms.PaintEventArgs>(MouseClick_ToDrawLine); }
+        }
 
-                line.Stroke = SystemColors.WindowFrameBrush;
-                line.X1 = (double) _mousePosition[0];
-                line.Y1 = (double) _mousePosition[1];
-                line.X2 = (double) _mousePosition[2];
-                line.Y2 = (double) _mousePosition[3];
+        private ObservableCollection<Line> managedLinePoints;
+        public ObservableCollection<Line> ManagedLinePoints
+        {
+            get { return managedLinePoints; }
+            set
+            {
+                managedLinePoints = value;
+                OnPropertyChanged("ManagedLinePoints");
+            }
+        }
+
+        private void MouseClick_ToDraw(EventArgs args)
+        {
+            MouseEventArgs e = (MouseEventArgs) args;
+
+            if(e.LeftButton == MouseButtonState.Released) // 
+            {
+                if (TestArrayPosition(mousePosition) == true)
+                {
+                    var position = new Point();
+                    position = e.GetPosition(e.Device.Target);
+                    mousePosition[0] = position;
+                    //count++;
+                    Console.WriteLine(mousePosition[0].X + " " + mousePosition[0].Y + " erster Punkt");
+                }
+                else
+                {
+                    var position = new Point();
+                    position = e.GetPosition(e.Device.Target);
+                    mousePosition[1] = position;
+                    //count++;
+                    Console.WriteLine(mousePosition[1].X + " " + mousePosition[1].Y + " zweiter Punkt");
+                    LineToDraw(mousePosition);
+
+                    mousePosition[0] = new Point(0,0);
+                    mousePosition[1] = new Point(0,0);
+                }  
+            }
+        }
+
+        private void MouseClick_ToDrawLine(System.Windows.Forms.PaintEventArgs painargs)
+        {
+            //System.Windows.Forms.PaintEventArgs graphics = (System.Windows.Forms.PaintEventArgs) args;
+            painargs.Graphics.DrawLine(pen, (float)mousePosition[0].X, (float)mousePosition[0].Y, (float)mousePosition[1].X, (float)mousePosition[1].Y);
 
         }
+
+        private Boolean TestArrayPosition(Point[] arrayPosition)
+        {
+            if (mousePosition[0] == arrayPosition[1])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Line LineToDraw(Point[] point)
+        {
+            Console.WriteLine(point[0].X + " " + point[0].Y + " " + point[1].X + " " + point[1].Y + " Punkt zum Zeichnen der Linie");
+            Line line = new Line();
+
+            line.StrokeThickness = 3;
+            line.Visibility = Visibility.Visible;
+            line.Stroke = new SolidColorBrush(Colors.Black);
+            line.X1 = point[0].X;
+            line.Y1 = point[0].Y;
+            line.X2 = point[1].X;
+            line.Y2 = point[1].Y;
+
+            return line;
+        }
+        public Point point { get; set; }
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+        public int StrokeThickness { get; set; }
+
+        public Colors Stroke { get; set; }
+
+
     }
 }
+
+public class LineToDraw{
+
+
+}
+
+
